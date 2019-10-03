be785da2a75d3a72022d6f74b075280e84477dce
DarshanGowda0 <darshan.gowda008@gmail.com>
2019-10-03 11:13:52 -0700

    auth and access token refactored

--- api/src/api.js
+++ api/src/api.js
@@ -7,27 +7,6 @@ import cors from 'cors';
 import config from './config.js';
 import router from './app/routes.js';
 
-var APP_PORT = 8000;
-var PLAID_CLIENT_ID = config.keys.client_id;
-var PLAID_SECRET = config.keys.secret;
-var PLAID_PUBLIC_KEY = config.keys.key;
-var PLAID_ENV = config.keys.env;
-
-// We store the access_token in memory
-// In production, store it in a secure persistent data store
-var ACCESS_TOKEN = null;
-var PUBLIC_TOKEN = null;
-var ITEM_ID = null;
-
-var plaid = require('plaid');
-var client = new plaid.Client(
-    PLAID_CLIENT_ID,
-    PLAID_SECRET,
-    PLAID_PUBLIC_KEY,
-    plaid.environments[PLAID_ENV],
-    { version: '2018-05-22' }
-);
-
 // Initialize Express
 var app = express();
 
@@ -53,35 +32,6 @@ app.use('/', router(client, config));
 const server = app.listen(config.port, () => {
     const { address, port } = server.address();
     console.log(`Listening at http://${address}:${port}`);
-});
-
-app.get('/auth', function (request, response, next) {
-    response.render('index.ejs', {
-        PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
-        PLAID_ENV: PLAID_ENV,
-    });
-});
-
-app.post('/get_access_token', function (request, response, next) {
-    PUBLIC_TOKEN = request.body.public_token;
-    client.exchangePublicToken(PUBLIC_TOKEN, function (error,
-        tokenResponse) {
-        if (error != null) {
-            var msg = 'Could not exchange public_token!';
-            console.log(msg + '\n' + JSON.stringify(error));
-            return response.json({
-                error: msg
-            });
-        }
-        ACCESS_TOKEN = tokenResponse.access_token;
-        ITEM_ID = tokenResponse.item_id;
-        prettyPrintResponse(tokenResponse);
-        response.json({
-            access_token: ACCESS_TOKEN,
-            item_id: ITEM_ID,
-            error: false
-        });
-    });
 });
 
 export default app;
--- api/src/app/routes.js
+++ api/src/app/routes.js
@@ -1,15 +1,65 @@
 import { Router } from 'express';
 
-export default(redis, config)=>{
-    const router = Router();
+export default (redis, config) => {
+  const router = Router();
 
-    router.get('/', (req, res) => {
-		res.send({
-			version: '1.0',
-			url: req.url
-		})
+  var APP_PORT = 8000;
+  var PLAID_CLIENT_ID = config.keys.client_id;
+  var PLAID_SECRET = config.keys.secret;
+  var PLAID_PUBLIC_KEY = config.keys.key;
+  var PLAID_ENV = config.keys.env;
+
+  // We store the access_token in memory
+  // In production, store it in a secure persistent data store
+  var ACCESS_TOKEN = null;
+  var PUBLIC_TOKEN = null;
+  var ITEM_ID = null;
+
+  var plaid = require('plaid');
+  var plaidClient = new plaid.Client(
+    PLAID_CLIENT_ID,
+    PLAID_SECRET,
+    PLAID_PUBLIC_KEY,
+    plaid.environments[PLAID_ENV],
+    { version: '2018-05-22' }
+  );
+
+  router.get('/', (req, res) => {
+    res.send({
+      version: '1.0',
+      url: req.url
+    })
+  });
+
+  router.get('/auth', function (request, response, next) {
+    response.render('index.ejs', {
+      PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
+      PLAID_ENV: PLAID_ENV,
     });
+  });
 
-    return router;
+  router.post('/get_access_token', function (request, response, next) {
+    PUBLIC_TOKEN = request.body.public_token;
+    plaidClient.exchangePublicToken(PUBLIC_TOKEN, function (error,
+      tokenResponse) {
+      if (error != null) {
+        var msg = 'Could not exchange public_token!';
+        console.log(msg + '\n' + JSON.stringify(error));
+        return response.json({
+          error: msg
+        });
+      }
+      ACCESS_TOKEN = tokenResponse.access_token;
+      ITEM_ID = tokenResponse.item_id;
+      console.log(tokenResponse);
+      response.json({
+        access_token: ACCESS_TOKEN,
+        item_id: ITEM_ID,
+        error: false
+      });
+    });
+  });
+
+  return router;
 
 }
